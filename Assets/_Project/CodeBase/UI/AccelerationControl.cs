f42361c8294b9ccc6c3fcc4052ef92f9aeff34c5
Yevhen <jamesche.yc@gmail.com>
2024-06-04 21:05:19 +0300

    acceleration and reverse - implemented

---
+++ Assets/_Project/CodeBase/UI/AccelerationControl.cs
@@ -0,0 +1,54 @@
+﻿using CodeBase.Utilities;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace CodeBase.UI {
+  public class AccelerationControl : UIBehaviour {
+    [SerializeField]
+    private Button _accelerationButton;
+    [SerializeField]
+    private Button _slowingButton;
+    private readonly int _maxAcceleration = 3;
+    private readonly float _minAcceleration = 0.5f;
+    private readonly float _accelerationStep = 0.5f;
+
+    protected override void Awake() {
+      _accelerationButton.AddListener(Accelerate);
+      _slowingButton.AddListener(Slowing);
+    }
+
+    protected override void Start() => UpdateDisplay();
+
+    protected override void OnDestroy() {
+      _accelerationButton.RemoveListener(Accelerate);
+      _slowingButton.RemoveListener(Slowing);
+    }
+
+    private void UpdateDisplay() {
+      float timescale = Time.timeScale;
+
+      if (timescale >= _maxAcceleration) {
+        Time.timeScale = _maxAcceleration;
+        _accelerationButton.interactable = false;
+      }
+
+      if (timescale <= _minAcceleration) {
+        Time.timeScale = _minAcceleration;
+        _slowingButton.interactable = false;
+      }
+    }
+
+    private void Accelerate() {
+      Time.timeScale += _accelerationStep;
+      _slowingButton.interactable = true;
+      UpdateDisplay();
+    }
+
+    private void Slowing() {
+      Time.timeScale -= _accelerationStep;
+      _accelerationButton.interactable = true;
+      UpdateDisplay();
+    }
+  }
+}
